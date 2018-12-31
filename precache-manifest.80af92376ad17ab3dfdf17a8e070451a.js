a83a0c7278158981e564b43e313d07a3635309c1
VarunIrani <varunirani0505@gmail.com>
2018-12-31 13:07:37 +0530

    Foozam v0.0.2 Build

---
+++ precache-manifest.80af92376ad17ab3dfdf17a8e070451a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ab0b675c1d899f04ac6",
+    "url": "/static/js/main.7ab0b675.chunk.js"
+  },
+  {
+    "revision": "b909b846364479d68ca3",
+    "url": "/static/js/1.b909b846.chunk.js"
+  },
+  {
+    "revision": "10a10ff6128271ecfe77322fc762ce2d",
+    "url": "/index.html"
+  }
+];
