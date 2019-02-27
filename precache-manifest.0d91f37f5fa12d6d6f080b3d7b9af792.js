9aa5071242458a92b4ce28f9c0b9a14d85ba5a6c
Varun Irani <varunirani@MacBook-Pro.local>
2019-02-27 08:41:08 +0530

    Updates

---
+++ precache-manifest.0d91f37f5fa12d6d6f080b3d7b9af792.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a41216a2c8570b811c85",
+    "url": "/foozam/static/js/runtime~main.a41216a2.js"
+  },
+  {
+    "revision": "04a5b5b61961ba408ff0",
+    "url": "/foozam/static/js/main.04a5b5b6.chunk.js"
+  },
+  {
+    "revision": "c3a0dd0ab04e4e0199f8",
+    "url": "/foozam/static/js/1.c3a0dd0a.chunk.js"
+  },
+  {
+    "revision": "69b3a69aea8bd1d26a505ff275cdb853",
+    "url": "/foozam/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/foozam/precache-manifest.411321b6c66411b3b991398b3093e101.js"
+  "/foozam/precache-manifest.0d91f37f5fa12d6d6f080b3d7b9af792.js"
 );
 
 workbox.clientsClaim();
