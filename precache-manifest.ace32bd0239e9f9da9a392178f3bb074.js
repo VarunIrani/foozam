5149a43c5417c4777c35ad912327fb94c128175f
VarunIrani <varunirani0505@gmail.com>
2019-05-08 11:16:42 +0530

    Updates

---
+++ precache-manifest.ace32bd0239e9f9da9a392178f3bb074.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b555b75abff3c2ef6d2bf9768d70174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be2fd4a998d8c3abbe2a",
+    "url": "/static/js/2.d52f795b.chunk.js"
+  },
+  {
+    "revision": "119419d76de52c2de345",
+    "url": "/static/js/main.0f6abe73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92eff0fc7035ac76ee3d391bc8a3078f.js"
+  "/precache-manifest.ace32bd0239e9f9da9a392178f3bb074.js"
 );
 
 self.addEventListener('message', (event) => {
